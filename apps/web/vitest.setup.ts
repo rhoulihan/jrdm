bc0d55afaed9de3fc8a3393c4e457d6969a63fdd
Rick Houlihan <rick.houlihan@gmail.com>
2026-05-16 10:12:23 -0500

    feat(web): DiagramPane React Flow shell

--- apps/web/vitest.setup.ts
+++ apps/web/vitest.setup.ts
@@ -2,6 +2,13 @@ import "@testing-library/jest-dom/vitest";
 import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
 
+// Polyfill ResizeObserver for React Flow (not available in jsdom)
+global.ResizeObserver = class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+};
+
 afterEach(() => {
   cleanup();
 });
